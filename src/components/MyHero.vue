cbe2ba7c7fcb69698ff821ada6f9bd19fde72e18
kaijie26 <chenkaijie26@gmail.com>
2022-07-05 18:38:37 +0200

    myhero

---
+++ src/components/MyHero.vue
@@ -0,0 +1,18 @@
+<!-- HTML -->
+<template>
+    <img alt="Vue-Jumbotron" src="https://docs.microsoft.com/learn/achievements/vue-cli-components-social.png">
+</template>
+
+<!-- JS -->
+<script>
+    export default {
+        name: 'MyHero'
+        
+    }
+
+</script>
+
+<!-- CSS -->
+<style lang='scss' scoped>
+
+</style>
