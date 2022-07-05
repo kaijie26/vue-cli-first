41785fda4ed3374fef78224b6fcf59bce58a49fe
kaijie26 <chenkaijie26@gmail.com>
2022-07-05 18:33:08 +0200

    mytitle

---
+++ src/components/MyTitle.vue
@@ -0,0 +1,18 @@
+<!-- HTML -->
+<template>
+    <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<!-- JS -->
+<script>
+    export default {
+        name: 'MyTitle'
+        
+    }
+
+</script>
+
+<!-- CSS -->
+<style lang='scss' scoped>
+
+</style>
